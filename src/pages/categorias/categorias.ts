ae201717465039bce4f05d1c08292985192bd6a5
Luan Reis <luanreis2202@gmail.com>
2021-11-21 15:02:39 -0300

    Mostrando categorias

---
+++ src/pages/categorias/categorias.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { CategoriaService } from '../../services/domain/categoria.service';
+
+/**
+ * Generated class for the CategoriasPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-categorias',
+  templateUrl: 'categorias.html',
+})
+export class CategoriasPage {
+
+
+  bucketUrl : string = API_CONFIG.bucketBaseUrl;
+
+  items : CategoriaDTO;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoriaService: CategoriaService) {
+  }
+
+
+
+  ionViewDidLoad() {
+    this.categoriaService.findAll()
+    .subscribe( response => {
+      this.items = response;
+    },
+    error => {
+      console.log(error);
+        }
+    )
+    
+  }
+
+
+}
